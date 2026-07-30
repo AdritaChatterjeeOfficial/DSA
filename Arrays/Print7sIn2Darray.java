74af1d3622a988fea7e95b6dcee0405962b64698
Adrita Chatterjee <adritachatterjee.official@gmail.com>
2026-07-30 13:40:43 +0530

    Add files via upload

---
+++ Arrays/Print7sIn2Darray.java
@@ -0,0 +1,17 @@
+public class Print7sIn2Darray {
+    public static int printSevens(int array[][]){
+        int count = 0;
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[0].length; j++){
+                if(array[i][j] == 7){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int array[][] = { {4,7,8},{8,8,7} };
+        System.out.println(printSevens(array));
+    }
+}
